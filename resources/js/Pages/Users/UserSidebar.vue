337ca9d33c378b81dfd5836057ad74b59a064125
npquanh <npquanh30402@gmail.com>
2024-03-29 19:12:25 +0700

    Add tab highlighting

--- resources/js/Pages/Users/UserSidebar.vue
+++ resources/js/Pages/Users/UserSidebar.vue
@@ -1,22 +1,32 @@
 <template>
     <div class="list-group shadow-sm small mb-3">
         <div class="list-group-item text-bg-dark">Voting Actions</div>
-        <button class="list-group-item list-group-item-action"
-                @click="switchTab('RoomList')">Room List
+        <button
+            :class="{ 'list-group-item': true, 'list-group-item-action': true, 'active': currentTab === 'RoomList' }"
+            @click="switchTab('RoomList')"
+        >
+            Room List
         </button>
-        <Link :href="route('room.create')" class="list-group-item list-group-item-action">Create
-            Room
-        </Link>
+        <Link :href="route('room.create')" class="list-group-item list-group-item-action">Create Room</Link>
     </div>
     <div class="list-group shadow-sm small mb-3">
         <div class="list-group-item text-bg-dark">User Actions</div>
-        <button class="list-group-item list-group-item-action" @click="switchTab('Friend')">Friend
-            List
+        <button
+            :class="{ 'list-group-item': true, 'list-group-item-action': true, 'active': currentTab === 'Friend' }"
+            @click="switchTab('Friend')"
+        >
+            Friend List
         </button>
-        <button class="list-group-item list-group-item-action" @click="switchTab('MusicPlayerSettings')">Music Player
+        <button
+            :class="{ 'list-group-item': true, 'list-group-item-action': true, 'active': currentTab === 'MusicPlayerSettings' }"
+            @click="switchTab('MusicPlayerSettings')"
+        >
+            Music Player
         </button>
-        <button class="list-group-item list-group-item-action"
-                @click="switchTab('UserSettings')">
+        <button
+            :class="{ 'list-group-item': true, 'list-group-item-action': true, 'active': currentTab === 'UserSettings' }"
+            @click="switchTab('UserSettings')"
+        >
             Settings
         </button>
     </div>
@@ -25,11 +35,13 @@
 <script setup>
 import {Link} from "@inertiajs/vue3";
 import {route} from "ziggy-js";
+import {ref} from "vue";
 
-const emit = defineEmits(['switch-tab'])
+const emit = defineEmits(['switch-tab']);
+let currentTab = ref('RoomList');
 
 const switchTab = (tabName) => {
-    emit('switch-tab', tabName)
-}
-
+    currentTab.value = tabName;
+    emit('switch-tab', tabName);
+};
 </script>
--- resources/js/Pages/Voting/VotingRoom/BallotSidebar.vue
+++ resources/js/Pages/Voting/VotingRoom/BallotSidebar.vue
@@ -1,32 +1,33 @@
 <template>
     <div class="list-group shadow-sm small mb-3">
         <div class="list-group-item text-bg-dark">Actions</div>
-        <button class="list-group-item list-group-item-action"
+        <button class="list-group-item list-group-item-action" :class="{'active': currentTab === 'RoomDetails'}"
                 @click="switchTab('RoomDetails')">Room Details
         </button>
-        <button class="list-group-item list-group-item-action" @click="switchTab('DescriptionPage')">Room Description
+        <button class="list-group-item list-group-item-action" :class="{'active': currentTab === 'DescriptionPage'}"
+                @click="switchTab('DescriptionPage')">Room Description
         </button>
-        <button class="list-group-item list-group-item-action"
+        <button class="list-group-item list-group-item-action" :class="{'active': currentTab === 'QuestionsPage'}"
                 @click="switchTab('QuestionsPage')">Questions/Candidates
         </button>
 
-        <button class="list-group-item list-group-item-action"
+        <button class="list-group-item list-group-item-action" :class="{'active': currentTab === 'UpdateTitleDesc'}"
                 @click="switchTab('UpdateTitleDesc')">Change Title/Desc
         </button>
 
-        <button class="list-group-item list-group-item-action"
+        <button class="list-group-item list-group-item-action" :class="{'active': currentTab === 'UpdateTime'}"
                 @click="switchTab('UpdateTime')">Change Date/Time
         </button>
 
-        <button class="list-group-item list-group-item-action"
+        <button class="list-group-item list-group-item-action" :class="{'active': currentTab === 'VoterPage'}"
                 @click="switchTab('VoterPage')">Voters
         </button>
 
-        <button class="list-group-item list-group-item-action"
+        <button class="list-group-item list-group-item-action" :class="{'active': currentTab === 'AttachmentPage'}"
                 @click="switchTab('AttachmentPage')">Attachments
         </button>
 
-        <button class="list-group-item list-group-item-action"
+        <button class="list-group-item list-group-item-action" :class="{'active': currentTab === 'UpdatePassword'}"
                 @click="switchTab('UpdatePassword')">Password
         </button>
 
@@ -38,7 +39,7 @@
                 @click="switchTab('')">Extra Settings
         </button>
 
-        <button class="list-group-item list-group-item-action"
+        <button class="list-group-item list-group-item-action" :class="{'active': currentTab === 'VotingResult'}"
                 @click="switchTab('VotingResult')">Results
         </button>
 
@@ -51,12 +52,15 @@
 <script setup>
 import {Link} from "@inertiajs/vue3";
 import {route} from "ziggy-js"
+import {ref} from "vue";
 
 defineProps(['room']);
 
 const emit = defineEmits(['switch-tab'])
+let currentTab = ref('RoomDetails');
 
 const switchTab = (tabName) => {
+    currentTab.value = tabName;
     emit('switch-tab', tabName)
 }
 </script>
