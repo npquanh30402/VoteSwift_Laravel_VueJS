82fac75fb01c9d8d3b7961aaac066cc3a2444406
npquanh <npquanh30402@gmail.com>
2024-04-21 19:34:43 +0700

    Add simple feature previews on the index page.

---
+++ resources/js/Pages/Index/CardPreview.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="card flex-fill shadow-sm">
+        <div class="card-header">
+            <slot name="header"></slot>
+        </div>
+        <div class="card-body vstack justify-content-between">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,3 +1,124 @@
 <template>
     <h1 class="display-1 text-center">Welcome to VoteSwift</h1>
+
+    <div class="container mt-5">
+        <div class="row g-4 mb-5 align-items-stretch">
+            <!-- Real-Time Voting -->
+            <div class="col-md-4 d-flex">
+                <CardPreview>
+                    <div>
+                        <i class="bi bi-stopwatch" style="font-size: 2em"></i>
+                        <h5 class="card-title">Real-Time Voting</h5>
+                        <p class="card-text">
+                            Cast your votes and see the results in real-time,
+                            allowing you to instantly gauge public opinion.
+                        </p>
+                    </div>
+                    <button class="btn btn-primary">Start Voting</button>
+                </CardPreview>
+            </div>
+
+            <!-- Live Results -->
+            <div class="col-md-4 d-flex">
+                <CardPreview>
+                    <div>
+                        <i
+                            class="bi bi-bar-chart-line"
+                            style="font-size: 2em"
+                        ></i>
+                        <h5 class="card-title">Live Results</h5>
+                        <p class="card-text">
+                            Watch the results unfold as votes come in, with
+                            dynamic charts and graphs showing real-time trends.
+                        </p>
+                    </div>
+                    <button class="btn btn-primary">View Results</button>
+                </CardPreview>
+            </div>
+
+            <!-- Customizable Polls -->
+            <div class="col-md-4 d-flex">
+                <CardPreview>
+                    <div>
+                        <i class="bi bi-gear" style="font-size: 2em"></i>
+                        <h5 class="card-title">Customizable Polls</h5>
+                        <p class="card-text">
+                            Create polls with customizable question types to
+                            meet a variety of needs, from simple choices to
+                            complex surveys.
+                        </p>
+                    </div>
+                    <button class="btn btn-primary mt-4">Create a Poll</button>
+                </CardPreview>
+            </div>
+
+            <!-- Encrypted Data -->
+            <div class="col-md-4 d-flex">
+                <CardPreview>
+                    <div>
+                        <i
+                            class="bi bi-database-fill-lock"
+                            style="font-size: 2em"
+                        ></i>
+                        <h5 class="card-title">Encrypted Data</h5>
+                        <p class="card-text">
+                            Ensures all voting data is encrypted for security
+                            and confidentiality.
+                        </p>
+                    </div>
+                </CardPreview>
+            </div>
+
+            <!-- User Authentication -->
+            <div class="col-md-4 d-flex">
+                <CardPreview>
+                    <div>
+                        <i class="bi bi-shield-lock" style="font-size: 2em"></i>
+                        <h5 class="card-title">User Authentication</h5>
+                        <p class="card-text">
+                            Ensures secure login and user authentication for
+                            controlled access to the voting system.
+                        </p>
+                    </div>
+                    <button class="btn btn-primary mt-4">Register</button>
+                </CardPreview>
+            </div>
+
+            <!-- Admin Dashboard -->
+            <div class="col-md-4 d-flex">
+                <CardPreview>
+                    <div>
+                        <i class="bi bi-menu-button" style="font-size: 2em"></i>
+                        <h5 class="card-title">Admin Dashboard</h5>
+                        <p class="card-text">
+                            Provides administrators with tools to manage polls
+                            and monitor results.
+                        </p>
+                    </div>
+                    <button class="btn btn-primary mt-4">Admin</button>
+                </CardPreview>
+            </div>
+
+            <!-- Friendship and Private Chat -->
+            <div class="col-md-4 d-flex">
+                <CardPreview>
+                    <div>
+                        <i class="bi bi-people" style="font-size: 2em"></i>
+                        <i class="bi bi-chat-dots" style="font-size: 2em"></i>
+                        <h5 class="card-title">Friendship and Private Chat</h5>
+                        <p class="card-text">
+                            Users can make friends within the app and have
+                            private chats for secure communication.
+                        </p>
+                    </div>
+                    <button class="btn btn-primary mt-4">
+                        Find a friend now!
+                    </button>
+                </CardPreview>
+            </div>
+        </div>
+    </div>
 </template>
+<script setup>
+import CardPreview from "@/Pages/Index/CardPreview.vue";
+</script>
